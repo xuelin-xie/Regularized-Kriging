842aeeea4ce1ae75ad0b24cf67d8e720d3faf291
xuelin-xie <85014785+xuelin-xie@users.noreply.github.com>
2023-11-01 11:31:32 +0800

    Add files via upload

---
+++ LPeTKfold.m
@@ -0,0 +1,35 @@
+function   mse  = LPeTKfold(S,Y,k,lambda)
+%%  K-fold cross validation
+[~,dim]=size(S);
+theta = 10; 
+lob = 1e-2;
+upb = 1e+2;
+theta = repmat(theta, 1, dim) ;
+lob = repmat(lob, 1, dim);
+upb =  repmat(upb, 1, dim);
+[M,N] = size(S);
+indices = crossvalind('Kfold',size(S,1),k);
+mse=0;
+
+for i=1:k
+test = (indices == i); 
+train = ~test;
+train_S = S(train,:);
+train_Y = Y(train,:);
+test_S = S(test,:);
+test_Y = Y(test,:);
+
+if isempty([test_S]) == 1
+    Error=sprintf('Need to change the initial point\nleast squares problem is underdetermined');
+    error(Error)
+end
+
+dmodel =lassopetdacefit(train_S, train_Y, @regpoly1, @corrgauss, theta, lob, upb, lambda);
+pred = predictor(test_S, dmodel);
+mse1 = sum((test_Y -pred).*(test_Y-pred))/size(pred,1);
+mse=mse+mse1;
+end 
+mse=mse/k;
+
+end
+
