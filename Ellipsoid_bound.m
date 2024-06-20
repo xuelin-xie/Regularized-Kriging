f6ca086bee0b2cacf2af50c4865be88f2a15a8cc
xuelin-xie <85014785+xuelin-xie@users.noreply.github.com>
2024-06-20 20:33:58 +0800

    Add files via upload

---
+++ Ellipsoid_bound.m
@@ -0,0 +1,5 @@
+function [lx,lu]=Ellipsoid_bound
+d=18;
+lx=0*ones(1,d);
+lu=1*ones(1,d);  % 0.6
+end
---
+++ stybtang_bound.m
@@ -0,0 +1,5 @@
+function [lx,lu]= stybtang_bound
+d=24;
+lx=0*ones(1,d);
+lu=0.5*ones(1,d);
+end
